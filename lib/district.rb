ee0ec9260fc9d121ab2a48c40509701e2a7dbb12
ToniRib <tonimarierib@gmail.com>
2015-11-12 14:18:31 -0700

    Reworking the load_data to accomodate the statewide test

--- lib/district.rb
+++ lib/district.rb
@@ -1,6 +1,6 @@
 class District
   attr_reader :name
-  attr_accessor :enrollment
+  attr_accessor :enrollment, :statewide_test
 
   def initialize(name_hash)
     @name = name_hash[:name].upcase
--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -15,34 +15,22 @@ class DistrictRepository
     @statewide_test_repo = StatewideTestRepository.new
   end
 
+  def district_names_across_repositories
+    enroll_names = @enrollment_repo.enrollments.keys
+    statewidetest_names = @statewide_test_repo.statewide_tests.keys
+    (enroll_names | statewidetest_names).map{|n| n.upcase}
+  end
+
   def load_data(options)
     options = hash_leaves_go_empty_hashes(options)
-    # options = {
-    #   :enrollment => {
-    #     :kindergarten => "./test/fixtures/kindergarten_tester.csv",
-    #     :high_school_graduation => "./test/fixtures/highschool_grad_tester.csv"
-    #   },
-    #   :statewide_testing => {
-    #     :third_grade => "./test/fixtures/third_grade_tester.csv",
-    #     :math => "./test/fixtures/math_average_proficiency_tester.csv"
-    #   }
-    # }
-    # data = KindergartenParser.new.parse(options[:enrollment][:kindergarten])
-    # later iterations must change this.
 
     @enrollment_repo.load_data(:enrollment => options[:enrollment])
     @statewide_test_repo.load_data(:statewide_testing => options[:statewide_testing])
 
-    enroll_names = @enrollment_repo.enrollments.keys
-    statewidetest_names = @statewide_test_repo.statewide_tests.keys
-
-    names = enroll_names | statewidetest_names
-
-    @enrollment_repo.enrollments.keys.each do |district_name|
-      @districts[district_name.upcase] = District.new(name: district_name)
-      enrollment = @enrollment_repo.enrollments[district_name.upcase]
-      @districts[district_name.upcase].enrollment = enrollment
-
+    district_names_across_repositories.each do |name|
+      @districts[name] = District.new(name: name)
+      @districts[name].enrollment = @enrollment_repo.find_by_name(name)
+      @districts[name].statewide_test = @statewide_test_repo.find_by_name(name)
     end
   end
 
