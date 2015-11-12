e4e6b6e446db35b08d5cf03fb26a50af9a50316c
Aaron Greenspan <afg419@gmail.com>
2015-11-12 10:22:43 -0700

    feat, test: have a year math/reading/writing percent parser

--- lib/year_mrw_percent_parser.rb
+++ lib/year_mrw_percent_parser.rb
@@ -2,17 +2,20 @@ require 'csv'
 require 'pry'
 require_relative 'data_formattable'
 
-class YearPercentParser
+class YearMRWPercentParser
   include DataFormattable
 
   def parse(ruby_rows)
     data = {}
     ruby_rows.each do |csv_row|
       year = csv_row.row_data[:timeframe].to_i
+      subject = csv_row.row_data[:score].to_s
+
       row_data = convert_to_float(csv_row.row_data[:data])
 
       data[csv_row.row_data[:location]] ||= {}
-      data[csv_row.row_data[:location]][year] = row_data
+      data[csv_row.row_data[:location]][year] ||= {}
+      data[csv_row.row_data[:location]][year][subject] = row_data
     end
 
     data
---
+++ test/year_mrw_percent_parser_test.rb
@@ -0,0 +1,85 @@
+require 'minitest'
+require 'year_mrw_percent_parser'
+
+class YearMRWPercentParserTest < Minitest::Test
+  def parser_prep
+    pre = Preprocessor.new
+    pre.pull_from_CSV('./test/fixtures/third_grade_tester.csv')
+  end
+
+  def test_class_exists
+    assert YearMRWPercentParser
+  end
+
+  def test_breaks_data_by_district
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+
+    expected = ['Colorado', 'ACADEMY 20', 'ADAMS COUNTY 14']
+
+    assert_equal expected, data.keys
+  end
+
+  def test_breaks_data_by_year_for_each_district
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = [2008, 2009, 2010, 2011, 2012, 2013, 2014]
+
+    assert_equal expected, data['Colorado'].keys.sort
+  end
+
+  def test_can_find_data_by_location_year_and_subject
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = 0.849
+
+    assert_equal expected, data['ACADEMY 20'][2010]["Math"]
+  end
+
+  def test_can_find_data_by_location_and_year_expect_na
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = 'N/A'
+
+    assert_equal expected, data['ACADEMY 20'][2008]["Writing"]
+  end
+
+  def test_can_find_data_by_location_and_year_expect_na_with_div_zero
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = 'N/A'
+
+    assert_equal expected, data['ADAMS COUNTY 14'][2010]["Math"]
+  end
+
+  def test_can_find_multiple_pieces_of_data
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected1 = 0.698
+    expected2 = 0.476
+
+    assert_equal expected1, data['Colorado'][2010]["Reading"]
+    assert_equal expected2, data['ADAMS COUNTY 14'][2011]["Math"]
+  end
+
+  def test_loads_data_correctly
+    parser = YearMRWPercentParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+
+    examined = ruby_rows[rand(2..60)]
+    location = examined.row_data[:location]
+    subject = examined.row_data[:score].to_s
+    year = examined.row_data[:timeframe].to_i
+
+    expected = parser.convert_to_float(examined.row_data[:data])
+
+    assert_equal expected, data[location][year][subject]
+  end
+end
