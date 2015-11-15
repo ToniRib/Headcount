137846663455d10a177504021b429bcbf3119598
ToniRib <tonimarierib@gmail.com>
2015-11-15 15:20:50 -0700

    refactor: clean up long lines in post_processor

--- lib/post_processor.rb
+++ lib/post_processor.rb
@@ -7,32 +7,55 @@ class PostProcessor
 
   include DataFormattable
 
-  def transpose_data(data)
-    data_transpose = Hash.new{ |h, k| h[k] = {} }
-
-    data.each_pair do |type, district|
-      district.to_h.each_pair{ |name, d| data_transpose[name][type] = d }
-    end
-
-    data_transpose
-  end
-
   def get_enrollment_data(options)
-    data = { :kindergarten_participation => get_year_percent_data(options[:enrollment][:kindergarten]),
-      :high_school_graduation => get_year_percent_data(options[:enrollment][:high_school_graduation])
+    data = {
+      kindergarten_participation: get_year_percent_data(kdata(options)),
+      high_school_graduation: get_year_percent_data(hsdata(options))
     }
+
     transpose_data(data)
   end
 
   def get_statewide_testing_data(options)
     options = hash_leaves_go_empty_hashes(options)
+
     data = {
-      :third_grade_proficiency => get_year_mrw_percent_data(options[:statewide_testing][:third_grade]),
-      :eighth_grade_proficiency => get_year_mrw_percent_data(options[:statewide_testing][:eighth_grade]),
-      :math => get_year_race_percent_data(options[:statewide_testing][:math]),
-      :reading => get_year_race_percent_data(options[:statewide_testing][:reading]),
-      :writing => get_year_race_percent_data(options[:statewide_testing][:writing])}
+      third_grade_proficiency: get_year_mrw_percent_data(third_data(options)),
+      eighth_grade_proficiency: get_year_mrw_percent_data(eighth_data(options)),
+      math: get_year_race_percent_data(math_data(options)),
+      reading: get_year_race_percent_data(reading_data(options)),
+      writing: get_year_race_percent_data(writing_data(options))
+    }
+
     transpose_data(data)
+  end
+
+  def kdata(options)
+    options[:enrollment][:kindergarten]
+  end
+
+  def hsdata(options)
+    options[:enrollment][:high_school_graduation]
+  end
+
+  def third_data(options)
+    options[:statewide_testing][:third_grade]
+  end
+
+  def eighth_data(options)
+    options[:statewide_testing][:eighth_grade]
+  end
+
+  def math_data(options)
+    options[:statewide_testing][:math]
+  end
+
+  def reading_data(options)
+    options[:statewide_testing][:reading]
+  end
+
+  def writing_data(options)
+    options[:statewide_testing][:writing]
   end
 
   def get_year_percent_data(file)
