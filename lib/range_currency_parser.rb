29e022b13bb17e2823e6e2aa6024b4ee7e2fdc75
Aaron Greenspan <afg419@gmail.com>
2015-11-16 18:51:16 -0700

    feat: added tester files for ec_profile files, and ec_profile class.  All ec_profile parsers done

---
+++ lib/range_currency_parser.rb
@@ -0,0 +1,21 @@
+require 'csv'
+require 'pry'
+require_relative 'data_formattable'
+
+class RangeCurrencyParser
+  include DataFormattable
+
+  def parse(ruby_rows)
+    data = {}
+
+    ruby_rows.each do |csv_row|
+      year = csv_row.row_data[:timeframe].split("-").map(&:to_i)
+      row_data = convert_to_float(csv_row.row_data[:data])
+
+      data[csv_row.row_data[:location]] ||= {}
+      data[csv_row.row_data[:location]][year] = row_data
+    end
+
+    data
+  end
+end
---
+++ test/range_currency_parser_test.rb
@@ -0,0 +1,41 @@
+require 'minitest'
+require 'range_currency_parser'
+
+class RangeCurrencyParserTest < Minitest::Test
+  def parser_prep
+    pre = Preprocessor.new
+    pre.pull_from_csv('./test/fixtures/median_household_tester.csv')
+  end
+
+  def test_class_exists
+    assert RangeCurrencyParser
+  end
+
+  def test_breaks_data_by_district
+    parser = RangeCurrencyParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = ['Colorado', 'ACADEMY 20', 'ADAMS COUNTY 14',
+                'ADAMS-ARAPAHOE 28J','AGATE 300','AGUILAR REORGANIZED 6']
+
+    assert_equal expected, data.keys
+  end
+
+  def test_breaks_data_by_year_for_each_district
+    parser = RangeCurrencyParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+    expected = [[2005, 2009], [2006, 2010], [2007, 2011],
+                [2008, 2012], [2009, 2013]]
+
+    assert_equal expected, data['Colorado'].keys.sort
+  end
+
+  def test_can_find_data_by_location_and_range
+    parser = RangeCurrencyParser.new
+    ruby_rows = parser_prep
+    data = parser.parse(ruby_rows)
+
+    assert_equal 56456.0, data['Colorado'][[2006, 2010]]
+  end
+end
