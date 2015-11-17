b4c6d97978d61bd9d2281ebaf8f4ace092dbbaf2
ToniRib <tonimarierib@gmail.com>
2015-11-17 16:26:11 -0700

    feat: ChildrenInPoverty adds children_in_poverty_in_year(year) method

--- lib/children_in_poverty.rb
+++ lib/children_in_poverty.rb
@@ -1,11 +1,11 @@
 require_relative 'data_formattable'
-require_relative 'data_calculatable'
+require_relative 'unknown_data_error'
+require 'pry'
 
 class ChildrenInPoverty
   attr_reader :name, :data
 
   include DataFormattable
-  include DataCalculatable
 
   def initialize(options)
     @name = options[:name]
@@ -13,7 +13,14 @@ class ChildrenInPoverty
   end
 
   def children_in_poverty_in_year(year)
+    fail UnknownDataError unless year_and_percent_exist(year)
 
+    percent = data[year][:>].fetch(:percent)
+
+    truncate_value(percent)
   end
 
+  def year_and_percent_exist(year)
+    data.keys.include?(year) && !data[year][:>][:percent].nil?
+  end
 end
