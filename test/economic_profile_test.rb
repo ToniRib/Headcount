40682365f251f871a29ccc5f7def7815e1f2d816
Aaron Greenspan <afg419@gmail.com>
2015-11-17 14:01:17 -0700

    test: economicprof_repo tests finished atm

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -2,7 +2,7 @@ require 'minitest'
 require 'economic_profile_repository'
 
 class EconomicProfileRepositoryTest < Minitest::Test
-  def load_statewide_repo
+  def load_profile_data
   ep = EconomicProfileRepository.new
   ep.load_data({
     :economic_profile => {
@@ -18,72 +18,68 @@ class EconomicProfileRepositoryTest < Minitest::Test
   end
 
   def test_load_data_creates_statewide_objects
-    skip
-  ep = load_statewide_repo
-    assert_equal 3, ep.statewide_tests.count
+    ep = load_profile_data
+    assert_equal 6, ep.profiles.count
 
-    ep.statewide_tests.each do |name, value|
-      assert value.is_a?(StatewideTest)
+    ep.profiles.each do |name, value|
+      assert value.is_a?(EconomicProfile)
     end
 
-    expected_keys = ['COLORADO', 'ACADEMY 20', 'ADAMS COUNTY 14']
-    assert_equal expected_keys, ep.statewide_tests.keys
+    expected_keys = ["COLORADO", "ACADEMY 20", "ADAMS COUNTY 14",
+                     "ADAMS-ARAPAHOE 28J", "AGATE 300", "AGUILAR REORGANIZED 6"]
+    assert_equal expected_keys, ep.profiles.keys
   end
 
   def test_can_find_by_district_name
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
+    profile = ep.find_by_name('ACADEMY 20')
 
-    assert_equal 'ACADEMY 20', ep.find_by_name('ACADEMY 20').name
+    assert_equal 'ACADEMY 20', profile.name
+    assert profile.is_a?(EconomicProfile)
   end
 
   def test_can_find_by_other_district_name
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
 
     assert_equal 'COLORADO', ep.find_by_name('Colorado').name
   end
 
   def test_gives_error_if_district_does_not_exist
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
 
     refute ep.find_by_name('XYZ')
   end
 
   def test_returns_true_if_district_exists
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
 
     assert ep.district_exists?('Colorado')
   end
 
   def test_returns_false_if_district_does_not_exist
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
 
     refute ep.district_exists?('XYZ')
   end
 
   def test_loaded_enrollment_objects_have_expected_names
-    skip
-  ep = load_statewide_repo
+    ep = load_profile_data
 
-    assert_equal 'COLORADO', ep.statewide_tests['COLORADO'].name
+    assert_equal 'COLORADO', ep.profiles['COLORADO'].name
   end
 
-  def test_loaded_statewide_objects_have_expected_subject_third_grade_data
-    skip
-  ep = load_statewide_repo
-    data = ep.statewide_tests['COLORADO'].proficient_for_subject_by_grade_in_year(:math, 3, 2012)
+  def test_loaded_statewide_objects_have_expected_median_data
+    ep = load_profile_data
 
-    assert_equal 0.710, data
+    data = ep.profiles['ACADEMY 20'].median.data[[2005,2009]]
+
+    assert_equal 85060.0, data
   end
 
   def test_loaded_statewide_objects_have_expected_subject_eight_grade_data
     skip
-  ep = load_statewide_repo
-    data = ep.statewide_tests['ACADEMY 20'].proficient_for_subject_by_grade_in_year(:reading, 8, 2013)
+    ep = load_profile_data
+    data = ep.profiles['ACADEMY 20'].proficient_for_subject_by_grade_in_year(:reading, 8, 2013)
     assert_equal 0.852, data
   end
 end
